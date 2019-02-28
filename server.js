82e37dd47f988dc7d9d1caaf99e8813ab0077392
ranand16 <ranand16@gmail.com>
2019-02-28 23:11:37 +0530

    added frontend fully

--- server.js
+++ server.js
@@ -18,6 +18,26 @@ app.all('/*',function(req,res){
       <head>\
           <base href="/">\
           <title></title\
+          @font-face {\
+            font-family: "myfont";\
+            src: url("fa2772327f55d8198301fdb8bcfc8158.woff") format("woff");\
+          }\
+          @font-face {\
+            font-family: "myfont";\
+            src: url("f4769f9bdb7466be65088239c12046d1.eot") format("woff");\
+          }\
+          @font-face {\
+            font-family: "myfont";\
+            src: url("e18bbf611f2a2e43afc071aa2f4e1512.ttf") format("woff");\
+          }\
+          @font-face {\
+            font-family: "myfont";\
+            src: url("89889688147bd7575d6327160d64e760.svg") format("woff");\
+          }\
+          @font-face {\
+            font-family: "myfont";\
+            src: url("448c34a56d699c29117adc64c43affeb.woff2") format("woff");\
+          }\
       </head>\
       <body>\
         <div ui-view><h1></h1></div>\
