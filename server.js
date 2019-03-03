57fd77b1c247d6c657434d8c5f42891548379ccb
ranand16 <ranand16@gmail.com>
2019-03-03 08:35:20 +0530

    uploading html to test api req. doing this because of CORS issue not fixing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var routes = require('./server/routes');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(function (req, res, next) {
   // Website you wish to allow to connect // * for all the websites
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,42 +25,44 @@ var PORT = process.env.PORT || 3000;
 // var ip = process.env.IP || '0.0.0.0';
 app.use(express.static('public'));
 // to overcome the CORS error.
-
-app.all('/*',function(req,res){
-  res.send('\
-    <!doctype html>\
-    <html>\
-      <head>\
-          <base href="/">\
-          <title></title\
-          @font-face {\
-            font-family: "myfont";\
-            src: url("fa2772327f55d8198301fdb8bcfc8158.woff") format("woff");\
-          }\
-          @font-face {\
-            font-family: "myfont";\
-            src: url("f4769f9bdb7466be65088239c12046d1.eot") format("woff");\
-          }\
-          @font-face {\
-            font-family: "myfont";\
-            src: url("e18bbf611f2a2e43afc071aa2f4e1512.ttf") format("woff");\
-          }\
-          @font-face {\
-            font-family: "myfont";\
-            src: url("89889688147bd7575d6327160d64e760.svg") format("woff");\
-          }\
-          @font-face {\
-            font-family: "myfont";\
-            src: url("448c34a56d699c29117adc64c43affeb.woff2") format("woff");\
-          }\
-      </head>\
-      <body>\
-        <div ui-view><h1></h1></div>\
-        <script type="text/javascript" src="bundle.js"></script>\
-      </body>\
-    </html>\
-    ');
+app.all('/*', function(req, res){
+  res.sendFile('inndex.html', {root: __dirname });
 });
+// app.all('/*',function(req,res){
+//   res.send('\
+//     <!doctype html>\
+//     <html>\
+//       <head>\
+//           <base href="/">\
+//           <title></title\
+//           @font-face {\
+//             font-family: "myfont";\
+//             src: url("fa2772327f55d8198301fdb8bcfc8158.woff") format("woff");\
+//           }\
+//           @font-face {\
+//             font-family: "myfont";\
+//             src: url("f4769f9bdb7466be65088239c12046d1.eot") format("woff");\
+//           }\
+//           @font-face {\
+//             font-family: "myfont";\
+//             src: url("e18bbf611f2a2e43afc071aa2f4e1512.ttf") format("woff");\
+//           }\
+//           @font-face {\
+//             font-family: "myfont";\
+//             src: url("89889688147bd7575d6327160d64e760.svg") format("woff");\
+//           }\
+//           @font-face {\
+//             font-family: "myfont";\
+//             src: url("448c34a56d699c29117adc64c43affeb.woff2") format("woff");\
+//           }\
+//       </head>\
+//       <body>\
+//         <div ui-view><h1></h1></div>\
+//         <script type="text/javascript" src="bundle.js"></script>\
+//       </body>\
+//     </html>\
+//     ');
+// });
 app.listen(PORT, function(){
   console.log("Its running !! @ localhost:"+PORT);
 });
