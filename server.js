e663a62b3a82ca5ed3180ca4b7fe2b1cee39ef22
ranand16 <ranand16@gmail.com>
2019-03-03 22:30:27 +0530

    solution for fallback config

--- server.js
+++ server.js
@@ -5,28 +5,28 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect // * for all the websites
-  res.header("Access-Control-Allow-Origin", "*");
-  // Request methods you wish to allow
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  // Request headers you wish to allow
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.header("Access-Control-Allow-Credentials", true);
-  console.log('cors problem fixed');
-  // Pass to next layer of middleware
-  next();
-});
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect // * for all the websites
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // Request methods you wish to allow
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//   // Request headers you wish to allow
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.header("Access-Control-Allow-Credentials", true);
+//   console.log('cors problem fixed');
+//   // Pass to next layer of middleware
+//   next();
+// });
 routes(app);
 
 var PORT = process.env.PORT || 3000;
 // var ip = process.env.IP || '0.0.0.0';
 app.use(express.static('public'));
 // to overcome the CORS error.
-app.all('/*', function(req, res){
-  res.sendFile('index.html', {root: __dirname });
+app.all('*', function(req, res){
+  res.status(200).sendFile('index.html', {root: __dirname });
 });
 // app.all('/*',function(req,res){
 //   res.send('\
