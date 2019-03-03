595cc61e86bcd865c0d835c2f71d4beaa8ffa6b8
ranand16 <ranand16@gmail.com>
2019-03-03 08:24:35 +0530

    cors issue fix 4

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.static('public'));
 // to overcome the CORS error.
 app.use(function (req, res, next) {
   // Website you wish to allow to connect // * for all the websites
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   // Request headers you wish to allow
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Credentials", true);
   console.log('cors problem fixed');
   // Pass to next layer of middleware
   next();
