ca6581f4108dd133309315f57b40ec7225572e3d
ranand16 <ranand16@gmail.com>
2019-03-04 08:18:45 +0530

    commit after adding signin component 5

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/**
+ ** @author <Rishabh Anand>
+ ** @email <ranand16@gmail.com>
+**/
+
 var express = require('express');
 var app = express();
 var routes = require('./server/routes');
@@ -5,64 +10,19 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect // * for all the websites
-//   res.header("Access-Control-Allow-Origin", "*");
-//   // Request methods you wish to allow
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-//   // Request headers you wish to allow
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.header("Access-Control-Allow-Credentials", true);
-//   console.log('cors problem fixed');
-//   // Pass to next layer of middleware
-//   next();
-// });
 routes(app);
 
 var PORT = process.env.PORT || 3000;
-// var ip = process.env.IP || '0.0.0.0';
+
 app.use(express.static('public'));
-// to overcome the CORS error.
+
+// this route will load an angular 6 application which is just for testing purpose.
+// the full code for this minified/distribution code can be found at 
+// https://github.com/ranand16/todoV2-angular6-application
 app.all('*', function(req, res){
   res.status(200).sendFile('index.html', {root: __dirname });
 });
-// app.all('/*',function(req,res){
-//   res.send('\
-//     <!doctype html>\
-//     <html>\
-//       <head>\
-//           <base href="/">\
-//           <title></title\
-//           @font-face {\
-//             font-family: "myfont";\
-//             src: url("fa2772327f55d8198301fdb8bcfc8158.woff") format("woff");\
-//           }\
-//           @font-face {\
-//             font-family: "myfont";\
-//             src: url("f4769f9bdb7466be65088239c12046d1.eot") format("woff");\
-//           }\
-//           @font-face {\
-//             font-family: "myfont";\
-//             src: url("e18bbf611f2a2e43afc071aa2f4e1512.ttf") format("woff");\
-//           }\
-//           @font-face {\
-//             font-family: "myfont";\
-//             src: url("89889688147bd7575d6327160d64e760.svg") format("woff");\
-//           }\
-//           @font-face {\
-//             font-family: "myfont";\
-//             src: url("448c34a56d699c29117adc64c43affeb.woff2") format("woff");\
-//           }\
-//       </head>\
-//       <body>\
-//         <div ui-view><h1></h1></div>\
-//         <script type="text/javascript" src="bundle.js"></script>\
-//       </body>\
-//     </html>\
-//     ');
-// });
+   
 app.listen(PORT, function(){
   console.log("Its running !! @ localhost:"+PORT);
 });
